daeb52011421486671666c7352d2785f8d0df447
Eugene Klimovich <pochta.korobka@gmail.com>
2021-05-10 00:32:34 +0300

    email sending added

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,33 @@
-let hamburger = document.getElementById("hamburger");
-let hamburgerAndLang = document.getElementById('hamburgerAndLang');
+(function() {
+  /**
+   * lang mobile menu
+   */
+  $('#hamburger').on('click', () => {
+    $('.menu-hamburger').toggleClass('active')
+    $('#hamburgerAndLang').toggleClass('active')
+  })
 
-hamburger.addEventListener("click", () => {
-  document.querySelector(".menu-hamburger").classList.toggle('active')
-  hamburgerAndLang.classList.toggle('active')
-});
+  /**
+   * feedback form submit
+   */
+  const $form = $('#userFeedback');
+
+  $form.on('submit', function(e) {
+    e.preventDefault()
+
+    const data = $(this).serializeArray().reduce((acc, item) => {
+      acc[item.name] = item.value
+
+      return acc
+    }, {})
+
+    $.ajax({
+      type: 'POST',
+      url: 'mail.php',
+      data,
+      success: function() {
+        $form.trigger('reset');
+      }
+    })
+  })
+})()
